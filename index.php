acee9bf960b91ba0bfb42de9ad0938f1bbd74f82
jimisvog <jimisvog@gmail.com>
2015-05-03 22:40:57 +0300

    The basic dropdown menu filled with places from .json file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 	<meta charset="UTF-8">
+	<title>DA WETHA</title>
+    <script type="text/javascript" src="js/fill.js"></script>
+
+	
+</head>
+<body>
+    <header>
+        <select name="Place" onchange="fill(this);">
+        <?php
+
+            
+
+            $string = file_get_contents("corfu_weather.json");
+            $json_a = json_decode($string, true);
+
+            foreach ($json_a["list"] as $place) {
+                echo "<option id=\"" . $place["id"] . "\">" . $place["name"] . "</option>";
+            }
+
+        ?>
+        </select>
+
+    </header>   
+    
+</body>
+
+</html>
---
+++ js/fill.js
@@ -0,0 +1,4 @@
+function fill(e){
+
+    alert(e.value);
+}
